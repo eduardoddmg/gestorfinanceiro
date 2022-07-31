b53d6dbceddfb242a35cdf49d7d2b9d2fc735e38
eduardoddmg <eduardoddmg@gmail.com>
2022-07-31 16:06:47 -0300

    up

--- backend/config/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var port = 3001;
-function configServer(app) {
-    app.listen(port, function () {
-        console.log("Server is running on port ".concat(port));
-    });
-}
-exports["default"] = configServer;
