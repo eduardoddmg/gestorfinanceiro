b75edd6b360e00d3da254fb00f2e4bdf470b63a7
eduardoddmg <eduardoddmg@gmail.com>
2022-07-31 11:43:03 -0300

    update setup backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,13 @@ import RouterMain from "./router";
 import { PortType } from "./types";
 const express = require('express');
 const app = express();
-
+const cors = require('cors');   
 const port: PortType = 3001;
 
+
 app.use(RouterMain());
+app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
