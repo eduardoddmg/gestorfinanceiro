54baf759eed7b042b346e9faf744d7c9dd300c6b
eduardoddmg <eduardoddmg@gmail.com>
2022-08-10 11:15:11 -0300

    fix cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 require("dotenv").config();
+
 const userRoute = require("./routes/user");
 const authRoute = require('./routes/auth');
 const transactionRoute = require('./routes/transaction');
