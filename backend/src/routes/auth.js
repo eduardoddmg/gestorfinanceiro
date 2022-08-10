ca9112f2ff734421dc4b89ac726545f634a31576
eduardoddmg <eduardoddmg@gmail.com>
2022-08-10 13:24:46 -0300

    fix backend

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -12,8 +12,9 @@ router.post('/createUser', async (req, res) => {
             console.log('console 1');
             const newUser = new userSchema(userData);
             console.log('console 2');
-            newUser.save().then(() => console.log('funcionou'));
-            res.status(200);
+            const user = await newUser.save();
+            console.log('console 3');
+            res.status(200).send({message: 'usuario criado com sucesso', type: 'success', data: user});
         } else {
             res.status(200).send({ message: 'usuario ja existe', type: 'error' });
         }
