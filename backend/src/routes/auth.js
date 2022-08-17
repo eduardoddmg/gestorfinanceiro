12c5afb21a5ca53dd57f09c7d55700090c2334ae
Pedro Ferreira <pedrovictor48@live.com>
2022-08-17 20:52:12 -0300

    Update auth.js
    bcrypt e login com jwt

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,38 +1,77 @@
-const userSchema = require('../models/user');
+const userSchema = require("../models/user");
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-router.post('/createUser', async (req, res) => {
-    try {
-        const userData = req.body;
-        const userBool = await userSchema.exists({ username: userData.username });
-        console.log(userBool);
-        if (!userBool) {
-            console.log('console 1');
-            const newUser = new userSchema(userData);
-            console.log('console 2');
-            const user = await newUser.save();
-            console.log('console 3');
-            res.status(200).send({ message: 'usuario criado com sucesso', type: 'success', data: user });
-        } else {
-            res.status(200).send({ message: 'usuario ja existe', type: 'error' });
-        }
-    } catch (error) {
-        res.status(500).send({ message: error });
+const saltRounds = 10;
+
+async function authUser(req, res, next) {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res
+            .status(400)
+            .json({ message: "Missing data in body request" });
     }
+
+    const bdUser = await userSchema.findOne({ email: email });
+
+    if (!bdUser)
+        return res.status(401).json({ message: "Invalid credentials" });
+
+    const check = await bcrypt.compare(password, bdUser.password);
+
+    if (!check) return res.status(401).json({ message: "Invalid credentials" });
+
+    req.userId = bdUser._id;
+    console.log(bdUser._id);
+    next();
+}
+
+function sendJWT(req, res) {
+    const token = jwt.sign({ id: req.userId }, process.env.SECRET, {
+        expiresIn: 300,
+    });
+
+    res.status(200).json({ token: token });
+}
+
+async function verifyJWT(req, res, next) {
+    const token = req.headers["x-access-token"];
+
+    if (!token) return res.status(401).json({ message: "No token provided" });
+
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err)
+            return res.status(500).json({ message: "Failed to authenticate" });
+
+        req.userId = decoded.id;
+        next();
+    });
+}
+
+router.post("/createUser", async (req, res) => {
+    const userData = req.body;
+
+    if (!userData.username || !userData.password || !userData.email) {
+        res.status(400).json({ message: "Missing data in body request" });
+    }
+
+    if (await userSchema.exists({ username: userData.username })) {
+        res.status(200).json({ message: "Username already in use" });
+    }
+
+    if (await userSchema.exists({ email: userData.email })) {
+        res.status(200).json({ message: "Email already in use" });
+    }
+
+    userData.password = await bcrypt.hash(userData.password, saltRounds);
+    const newUser = new userSchema(userData);
+    const user = await newUser.save();
+    res.status(201).json({ message: "User successfully created" });
 });
 
-router.get('/loginUser', async (req, res) => {
-    try {
-        const userData = req.query;
-        console.log(userData);
-        const userBool = await userSchema.exists({ username: userData.username, password: userData.password });
-        const userDataBd = await userSchema.findOne({ username: userData.username, password: userData.password });
-        userBool ? res.status(200).json({ message: 'Usuário logado com sucesso', isLogged: true, data: userDataBd }) : res.status(200).json({ message: 'Dados inválidos', isLogged: false });
-    } catch (error) {
-        res.status(500).json({ message: error });
-    }
-})
+router.get("/loginUser", authUser, sendJWT);
 
 module.exports = router;
