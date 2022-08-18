5fff3ca11d78ad2095a27dce595831fd3f7fc978
eduardoddmg <eduardoddmg@gmail.com>
2022-08-17 22:12:07 -0300

    fix status http

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -60,11 +60,11 @@ router.post("/createUser", async (req, res) => {
         }
 
         if (await userSchema.exists({ username: userData.username })) {
-            res.status(400).json({ type: 'error', message: "Username already in use" });
+            res.status(200).json({ type: 'error', message: "Username already in use" });
         }
 
         if (await userSchema.exists({ email: userData.email })) {
-            res.status(400).json({ type: 'error', message: "Email already in use" });
+            res.status(200).json({ type: 'error', message: "Email already in use" });
         }
         userData.password = await bcrypt.hash(userData.password, saltRounds);
         const newUser = new userSchema(userData);
